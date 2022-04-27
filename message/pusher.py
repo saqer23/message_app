c0117c394d4a99890815b2399ae534e78c392b0d
saqer23 <aljabrisaqer23@gmail.com>
2022-04-27 06:12:31 +0300

    initial commit

---
+++ message/pusher.py
@@ -0,0 +1,9 @@
+import pusher
+
+pusher_client = pusher.Pusher(
+  app_id='1360431',
+  key='9e3c16ac676e2cca536c',
+  secret='4b4025e06cc74391ef02',
+  cluster='us2',
+  ssl=True
+)
---
+++ message/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .pusher import pusher_client
+
+class MessagesAPIView(APIView):
+    def post(self,request):
+        pusher_client.trigger('chat', 'message', {
+            'username':request.data['username'],
+            'message':request.data['message'],
+        })
+        return Response([])
